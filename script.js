3e7bd4f689fbea59d696b1181d7d5861c525ee42
giselelopesf <giselelopesff@gmail.com>
2025-11-16 20:57:25 -0300

    fix image filename 2

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function toggleMode() {
   const img = document.querySelector("#profile-img")
 
   if (html.classList.contains("light")) {
-    img.setAttribute("src", "assets/avatar.png")
+    img.setAttribute("src", "assets/Avatar.png")
   } else {
     img.setAttribute("src", "assets/avatar-light.png")
   }
