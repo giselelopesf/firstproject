d4632c563788a6f7b66bd878fd62c34ea94db600
giselelopesf <giselelopesff@gmail.com>
2025-11-16 21:26:01 -0300

    name

--- script.js
+++ script.js
@@ -5,12 +5,11 @@ function toggleMode() {
   const img = document.querySelector("#profile-img")
 
   if (html.classList.contains("light")) {
-    img.setAttribute("src", "assets/Avatar.png")
-  } else {
-    img.setAttribute("src", "assets/avatar-light.png")
-  }
+  img.setAttribute("src", "ASSETS/Avatar.png")
+} else {
+  img.setAttribute("src", "ASSETS/avatar-light.png")
 }
-
+}
 
 
 
